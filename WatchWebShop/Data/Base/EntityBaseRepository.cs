b51d29a4393fc18a8b7f7399533a18d18639725e
Attila Krizso <ITN236568@qualifizierung.at>
2023-01-19 14:15:51 +0100

    IEntityBase, EntityBaseRepository, IEntityBaseRepository elkeszitve, de meg nem hasznalva!!!

---
+++ WatchWebShop/Data/Base/EntityBaseRepository.cs
@@ -0,0 +1,53 @@
+﻿using DocumentFormat.OpenXml.Vml.Office;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WatchWebShop.Data.Base
+{
+    public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+        private readonly AppDbContext _context;
+
+        public EntityBaseRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await _context.Set<T>().FirstOrDefaultAsync(x => x.Id == id);
+            EntityEntry entityEntry = _context.Entry<T>(entity);
+            entityEntry.State = EntityState.Deleted;
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            var result = await _context.Set<T>().ToListAsync();
+            return result;
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            var result = await _context.Set<T>().FirstOrDefaultAsync(x => x.Id == id);
+            return result;
+        }
+
+        public async Task UpdateAsync(int id, T entity)
+        {
+            EntityEntry entityEntry = _context.Entry<T>(entity);
+            entityEntry.State = EntityState.Modified;
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
