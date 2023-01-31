b6e952cd0f295b9b553abdced58a090b8d394e85
Attila Krizso <ITN236568@qualifizierung.at>
2023-01-31 08:08:05 +0100

    categories controller az Index-szel es a hozza tartozo viewval - kesobb megcsinalni az osszes logikat es viewt

---
+++ WatchWebShop/Controllers/CategoriesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using WatchWebShop.Data;
+
+namespace WatchWebShop.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly AppDbContext _context;
+        
+        public CategoriesController(AppDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            var allCategories = await _context.Categories.ToListAsync();
+            return View(allCategories);
+        }
+    }
+}
