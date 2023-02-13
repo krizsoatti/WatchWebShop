21b7aef137f7785fbb5f9bd7c4b11fd1a3536223
Attila Krizso <ITN236568@qualifizierung.at>
2023-02-13 09:57:32 +0100

    Categories logic und views fertig

--- WatchWebShop/Controllers/CategoriesController.cs
+++ WatchWebShop/Controllers/CategoriesController.cs
@@ -1,27 +1,111 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Linq;
 using System.Threading.Tasks;
 using WatchWebShop.Data;
+using WatchWebShop.Data.Services;
 using WatchWebShop.Data.Static;
+using WatchWebShop.Models;
 
 namespace WatchWebShop.Controllers
 {
     [Authorize(Roles = UserRoles.Admin)]
     public class CategoriesController : Controller
     {
-        private readonly AppDbContext _context;
+        private readonly ICategoriesService _service;
         
-        public CategoriesController(AppDbContext context)
+        public CategoriesController(ICategoriesService service)
         {
-            _context = context;
+            _service = service;
         }
 
         [AllowAnonymous]
         public async Task<IActionResult> Index()
         {
-            var allCategories = await _context.Categories.ToListAsync();
+            var allCategories = await _service.GetAllAsync();
             return View(allCategories);
+        }
+
+        //GET: Categories/Details/1
+        [AllowAnonymous]
+        public async Task<IActionResult> Details(int id)
+        {
+            var categoryDetails = await _service.GetByIdAsync(id);
+            if (categoryDetails == null)
+            {
+                return View("NotFound");
+            }
+            return View(categoryDetails);
+        }
+
+        //GET: Categories/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Name, TaxRate")] Category category)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(category);
+            }
+
+            await _service.AddAsync(category);
+            return RedirectToAction(nameof(Index));
+        }
+
+        //GET: Categories/Edit/1
+        public async Task<IActionResult> Edit(int id)
+        {
+            var categoryDetails = await _service.GetByIdAsync(id);
+            if (categoryDetails == null)
+            {
+                return View("NotFound");
+            }
+            return View(categoryDetails);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id, Name, TaxRate")] Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(category);
+            }
+
+            if (id == category.Id)
+            {
+                await _service.UpdateAsync(id, category);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        //GET: Categories/Delete/1
+        public async Task<IActionResult> Delete(int id)
+        {
+            var categoryDetails = await _service.GetByIdAsync(id);
+            if (categoryDetails == null)
+            {
+                return View("NotFound");
+            }
+            return View(categoryDetails);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var categoryDetails = await _service.GetByIdAsync(id);
+            if (categoryDetails == null)
+            {
+                return View("NotFound");
+            }
+            await _service.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
